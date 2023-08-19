2759526b0969a2511fafcd981745453510d255e1
Shreyansh Shukla <109788335+shreyansh1410@users.noreply.github.com>
2023-08-19 20:03:18 +0530

    updated files

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,34 +1,45 @@
 var string="0";
+var string2="0";
 var count=0;
 
 for(var i=0;i<document.querySelectorAll(".button").length;i++){
 
     document.querySelectorAll(".button")[i].addEventListener("click",function(){
         if(count==0 || string=="0"){
+            string2="";
             string="";
-            document.querySelectorAll(".input").value=string;
+            document.querySelectorAll(".input").value=string2;
         }
         var buttonInnerHTML  = this.innerHTML;
         if(buttonInnerHTML=="="){
             string = eval(string);
             document.querySelector("input").value=string;
         }
+        else if(buttonInnerHTML=="%"){
+            string+="/100";
+            string2+="%"
+            document.querySelector(".input").value=string2;
+        }
         else if(buttonInnerHTML=="AC"){
             string="0";
-            document.querySelector("input").value=string;
+            string2="0";
+            document.querySelector("input").value=string2;
         }
         else if(buttonInnerHTML=="x"){
             string+="*";
-            document.querySelector(".input").value=string;
+            string2+="x";
+            document.querySelector(".input").value=string2;
         }
         else if(buttonInnerHTML=="del"){
             string=string.substring(0,string.length-1);
-            document.querySelector(".input").value=string;
+            string2=string2.substring(0,string2.length-1);
+            document.querySelector(".input").value=string2;
         }
         else{
             // console.log(this.innerHTML);
             string+=this.innerHTML;
-            document.querySelector("input").value=string;
+            string2+=this.innerHTML;
+            document.querySelector("input").value=string2;
         }
         count++;
         console.log(string.length);
