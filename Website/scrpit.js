2c02cb16deda8844b8cc867e75463a50f9a7f03f
Shreyansh Shukla <109788335+shreyansh1410@users.noreply.github.com>
2023-08-31 22:06:16 +0530

    Added a new project folder for portfolio file

---
+++ Website/scrpit.js
@@ -0,0 +1,11 @@
+var typed = new Typed("#element", {
+    strings: ["Competitve Programmer", "Web Developer"],
+    typeSpeed: 75,
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const fadingImage = document.getElementById("fading-image");
+    fadingImage.style.opacity = "1";
+});
+
